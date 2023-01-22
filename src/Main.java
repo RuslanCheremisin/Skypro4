f2f43d0d981227db4824e97df549434c847dcc6d
rus <ruslan.cheremisin@gmail.com>
2023-01-23 00:13:50 +0200

    я вообще не понял первое задание, поэтому списал. Второе сделал сам, работает

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task2(List.of(2,3,5,6,31,6,8));
+    }
+    public static <T> void findMinMax(Stream<? extends T> stream,
+                                      Comparator<? super T> order,
+                                      BiConsumer<? super T, ? super T> minMaxConsumer){
+        minMaxConsumer.accept(stream.min(order).orElse(null),stream.max(order).orElse(null));
+
+    }
+    public static void task2(List<Integer> intList){
+        System.out.println(intList.stream()
+                .filter(i -> i % 2 == 0)
+                .count());
     }
 }
